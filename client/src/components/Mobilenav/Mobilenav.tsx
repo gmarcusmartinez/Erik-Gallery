e0b6309e387985af12a19f9ab1eac43cc8318518
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-06-10 12:03:26 -0700

    media queries: ClothingScreen

--- client/src/components/Mobilenav/Mobilenav.tsx
+++ client/src/components/Mobilenav/Mobilenav.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Link } from 'react-router-dom';
+import { navlinks } from '../../data';
 interface MobilenavProps {
   setDisplayMobilenav: Function;
 }
 
 const Mobilenav: React.FC<MobilenavProps> = ({ setDisplayMobilenav }) => {
+  let links = navlinks.map((l: any) => (
+    <Link
+      key={l.text}
+      to={`/${l.to}`}
+      onClick={() => setDisplayMobilenav(false)}
+    >
+      {l.text}
+    </Link>
+  ));
+
   return ReactDOM.createPortal(
     <div className='m-nav' onClick={() => setDisplayMobilenav(false)}>
-      <div className='m-nav__body' onClick={(e) => e.stopPropagation()}></div>
+      <div className='m-nav__body' onClick={(e) => e.stopPropagation()}>
+        <div className='links'>{links}</div>
+      </div>
     </div>,
     document.querySelector('#mobile-nav')!
   );
