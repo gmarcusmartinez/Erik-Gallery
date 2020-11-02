58043405ebbf299f913810578ba54cddf3de61c4
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-01 19:31:37 -0600

    Heroku test

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = require("./app");
-app_1.app.listen(5000, function () { return console.log("Server running on port 5000"); });
+var PORT = process.env.PORT || 5000;
+app_1.app.listen(PORT, function () { return console.log("Server running on port " + PORT); });
