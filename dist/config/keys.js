5fd7a23a0738b3d8a1fa21889eea6b5beb472054
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-03 16:44:50 -0600

    prepare for deployment

--- dist/config/keys.js
+++ dist/config/keys.js
@@ -3,11 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var dev_1 = __importDefault(require("./dev"));
+// import devKeys from "./dev";
 var prod_1 = __importDefault(require("./prod"));
 var keys;
 if (process.env.NODE_ENV === "production")
     keys = prod_1.default;
-else
-    keys = dev_1.default;
+// else keys = devKeys;
 exports.default = keys;
