418da2deb726aad782588534b1fadca3cb97f5b7
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-03 17:59:08 -0600

    test 5 for header and layout

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -20,7 +20,7 @@ const Navigation: React.FC<IProps> = ({ isOpen, toggleNav, currentUser }) => {
       <div className="header">
         <MenuBars bool={isOpen} cb={toggleNav} />
         <Link to="/" className="header__title">
-          Erik Felfalusi
+          Erik Felfalusa
         </Link>
         {/* <CartIcon /> */}
         {/* {hidden ? null : <CartDropDown />} */}
--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const MainLayout: React.FC = ({ children }) => {
+  const backgroundImage = `url(https://lh3.googleusercontent.com/rNpoeeAsGMe2wBbcMHJbsnixLLSdi0cwGR_BikTTSgTMGRPpqDAmn0bcmRJGsYAcOOxRHLycid7qWh9wRnOwo34vuddxxeh7XDqCDVYezqYr_7O4C7nPF83I1ZixAD1yvusI3AS-ww=w2400)`;
+
   return (
-    <div className="main-layout">
-      <div className="bg-image">{children}</div>
+    <div className="main-layout" style={{ backgroundImage }}>
+      {children}
     </div>
   );
 };
