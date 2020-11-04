792d43f504fe5f6db9b3199c168605b5b0d5eca3
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-03 21:33:50 -0600

    main-content bugs

--- client/src/screens/Contact/index.tsx
+++ client/src/screens/Contact/index.tsx
@@ -5,19 +5,21 @@ const Contact = () => {
   return (
     <MainLayout>
       <div className="contact-screen">
-        <p>Personal Email: erik.felfalusi@gmail.com</p>
-        <p>Booking Email: hereticsx555@gmail.com</p>
-        <p>Phone:+407 42233365</p>
-        <p>
-          Social:
-          <a
-            href="https://www.instagram.com/1000bodies/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Instagram
-          </a>
-        </p>
+        <div className="contact-screen__info">
+          <p>Personal Email: erik.felfalusi@gmail.com</p>
+          <p>Booking Email: hereticsx555@gmail.com</p>
+          <p>Phone:+407 42233365</p>
+          <p>
+            Social:
+            <a
+              href="https://www.instagram.com/1000bodies/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Instagram
+            </a>
+          </p>
+        </div>
       </div>
     </MainLayout>
   );
