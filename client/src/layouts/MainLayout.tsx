fe7b91842d6a9648b1e9d187295476530f7b2eac
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-03 19:44:47 -0600

    mobile main-content lock test

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -4,7 +4,8 @@ const MainLayout: React.FC = ({ children }) => {
   const backgroundImage = `url(https://lh3.googleusercontent.com/rNpoeeAsGMe2wBbcMHJbsnixLLSdi0cwGR_BikTTSgTMGRPpqDAmn0bcmRJGsYAcOOxRHLycid7qWh9wRnOwo34vuddxxeh7XDqCDVYezqYr_7O4C7nPF83I1ZixAD1yvusI3AS-ww=w2400)`;
 
   return (
-    <div className="main-layout" style={{ backgroundImage }}>
+    <div className="main-layout">
+      <div className="main-bg" style={{ backgroundImage }}></div>
       {children}
     </div>
   );
