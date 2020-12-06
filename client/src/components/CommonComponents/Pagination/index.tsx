0d0eeef8a539389091386d8606d6c18784aca538
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-12-06 17:36:50 -0600

    pagination and admin page style changes

--- client/src/components/CommonComponents/Pagination/index.tsx
+++ client/src/components/CommonComponents/Pagination/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import history from "core/history";
+import { useHistory } from "react-router-dom";
 
 interface IProps {
   root: string;
@@ -8,6 +8,8 @@ interface IProps {
 const Pagination: FC<IProps> = ({ root, pages }) => {
   const pageNumbers: any[] = [];
   for (let i = 1; i <= pages; i++) pageNumbers.push(i);
+  const history = useHistory();
+  const active = +history.location.pathname.split("/")[2];
 
   const handleClick = (p: number) => {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
@@ -17,7 +19,7 @@ const Pagination: FC<IProps> = ({ root, pages }) => {
   const renderPageNumbers = () => {
     return pageNumbers.map((p) => (
       <div className="page-number" key={p} onClick={() => handleClick(p)}>
-        {p}
+        <span className={p === active ? "active" : ""}>{p}</span>
       </div>
     ));
   };
