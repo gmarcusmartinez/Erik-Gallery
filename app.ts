e5e8e814452e1fef5066a82ffa7164e50a41ecd0
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-01-20 15:29:14 -0500

    Test Deploy Ready

--- app.ts
+++ app.ts
@@ -1,52 +1,52 @@
-import path from "path";
-import sslRedirect from "heroku-ssl-redirect";
-import express, { Request, Response } from "express";
-import "express-async-errors";
-import cookieSession from "cookie-session";
-import cors from "cors";
-import dotenv from "dotenv";
+import path from 'path';
+import sslRedirect from 'heroku-ssl-redirect';
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import { authRouter } from "./routes/auth";
-import { backgroundRouter } from "./routes/backgrounds";
-import { orderRouter } from "./routes/orders";
-import { printRouter } from "./routes/prints";
-import { uploadRouter } from "./routes/upload";
-import { zineRouter } from "./routes/zines";
+import { authRouter } from './routes/auth';
+import { backgroundRouter } from './routes/backgrounds';
+import { orderRouter } from './routes/orders';
+import { printRouter } from './routes/prints';
+import { uploadRouter } from './routes/upload';
+import { zineRouter } from './routes/zines';
 
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-import keys from "./config/keys";
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
+import keys from './config/keys';
 
 dotenv.config();
 
 const app = express();
-// app.use(sslRedirect());
+app.use(sslRedirect());
 app.use(express.json());
 app.use(cors());
 app.use(cookieSession({ signed: false, secure: false }));
 
-app.use("/api/auth", authRouter);
-app.use("/api/uploads", uploadRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/uploads', uploadRouter);
 
-app.use("/api/backgrounds", backgroundRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/prints", printRouter);
-app.use("/api/zines", zineRouter);
+app.use('/api/backgrounds', backgroundRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/prints', printRouter);
+app.use('/api/zines', zineRouter);
 
-app.get("/api/config/paypal", (req: Request, res: Response) =>
+app.get('/api/config/paypal', (req: Request, res: Response) =>
   res.send(keys.paypalClientID)
 );
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
-app.all("*", () => {
+app.all('*', () => {
   throw new NotFoundError();
 });
 
