10a5bb04c92893de4dd3077cdaa227208dea9b22
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-01-24 14:11:10 -0500

    Remove unused screens

--- client/src/screens/index.ts
+++ client/src/screens/index.ts
@@ -1,5 +1,4 @@
 export { default as LoginScreen } from './Login';
-export { default as CartScreen } from './Cart';
 export { default as ContactScreen } from './Contact';
 export { default as Dashboard } from './Dashboard';
 export { default as DashboardZine } from './DashboardZine';
@@ -7,10 +6,7 @@ export { default as LandingScreen } from './Landing';
 export { default as LogoutScreen } from './Logout';
 export { default as NotFoundScreen } from './NotFound';
 export { default as Order } from './Order';
-export { default as PaymentScreen } from './Payment';
 export { default as PrintsScreen } from './Prints';
-export { default as ReviewOrderScreen } from './ReviewOrder';
-export { default as ShippingScreen } from './Shipping';
 export { default as SoundScreen } from './Sound';
 export { default as SoldoutScreen } from './Soldout';
 export { default as ZinesScreen } from './Zines';
