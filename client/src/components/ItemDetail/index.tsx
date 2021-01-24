231933b8e7692acf7505fcde159bc225d2b88980
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-01-24 13:05:13 -0500

    Item Detail styles

--- client/src/components/ItemDetail/index.tsx
+++ client/src/components/ItemDetail/index.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import { s3Url } from 'api/url';
 import { useTypedSelector } from 'hooks/use-typed-selector';
-import { useActions } from 'hooks/use-actions';
-import { Select } from 'components/CustomInputs';
-import { IProduct } from 'interfaces';
-import { mapQuantityToOptions } from 'utils';
+import PlaceHolder from './PlaceHolder';
 
-const ItemDetail: React.FC = () => {
-  const [qty, setQty] = React.useState(1);
-  const { addItemToCart } = useActions();
+const ItemDetail = () => {
   const { data } = useTypedSelector(({ modal }) => modal);
-  const { description, title, mainImage, price, quantityInStock } = data!;
+  const { description, title, mainImage } = data!;
   const backgroundImage = `url(${s3Url}/${mainImage})`;
-  const options = mapQuantityToOptions(quantityInStock);
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
-    setQty(+e.target.value);
-
-  const handleAddToCart = (item: IProduct, qty: number) => {
-    const cartItem = { ...item, quantity: qty };
-    addItemToCart(cartItem);
-  };
 
   return (
     <div className='item-detail'>
@@ -28,24 +14,8 @@ const ItemDetail: React.FC = () => {
       <div className='item-detail__info'>
         {description && <p className='item-detail__text'>{description}</p>}
         {title && <p className='item-detail__text'>{title}</p>}
-        <p className='item-detail__text'>
-          <span>{price}&euro;</span>
-          <span>(vat Included)</span>
-        </p>
         <p className='item-detail__text'>{data.size ? data.size : null}</p>
-        <Select
-          label='Select Quantity'
-          name='qty'
-          value={qty}
-          onChange={handleChange}
-          options={options}
-        />
-        <div
-          className='item-detail__btn'
-          onClick={() => handleAddToCart(data, qty)}
-        >
-          Add to Cart
-        </div>
+        <PlaceHolder />
       </div>
     </div>
   );
--- client/src/components/PrintItem/ItemAttrs.tsx
+++ client/src/components/PrintItem/ItemAttrs.tsx
@@ -11,16 +11,25 @@ const ItemAttrs: React.FC<IProps> = ({ item }) => {
   const { mainImage } = item;
   const backgroundImage = `url(${s3Url}/${mainImage})`;
 
-  const { toggleLightbox } = useActions();
+  const { toggleLightbox, toggleModal } = useActions();
   const handleToggleLightbox = () => toggleLightbox(true, backgroundImage);
+
+  const handleToggleModal = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    toggleModal(true, 'VIEW_ITEM', item);
+  };
 
   return (
     <div className='item-attrs'>
       <div className='item-attrs__wrapper' onClick={handleToggleLightbox}>
         <p>{item.description}</p>
         <p>{item.size}</p>
+        <div className='item-attrs__btn' onClick={(e) => handleToggleModal(e)}>
+          View Product
+        </div>
       </div>
-      <div className='item-attrs__btn'>View Product</div>
     </div>
   );
 };
