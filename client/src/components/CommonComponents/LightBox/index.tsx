f8956f487366b9e998da60ed385d52d46a42dd85
Marcus Martinez <gmarcusmartinez@gmail.com>
2021-02-11 20:35:30 -0500

    Lightbox done with transitions

--- client/src/components/CommonComponents/LightBox/index.tsx
+++ client/src/components/CommonComponents/LightBox/index.tsx
@@ -4,41 +4,42 @@ import { useActions } from 'hooks/use-actions';
 import { useTypedSelector } from 'hooks/use-typed-selector';
 import { s3Url } from 'api/url';
 
-const Lightbox: React.FC = () => {
+const Lightbox = () => {
   const [current, setCurrent] = React.useState(0);
-  const { images, display, index } = useTypedSelector(
-    ({ lightbox }) => lightbox
-  );
   const { toggleLightbox } = useActions();
+  const { images, display, index } = useTypedSelector(
+    (state) => state.lightbox
+  );
+  const last = images.length - 1;
+  const prev = () => setCurrent(current === 0 ? last - 1 : current - 1);
+  const next = () => setCurrent(current === last - 1 ? 0 : current + 1);
+  const handleCloseLightbox = () => toggleLightbox(false, [], current);
 
-  const prev = () => {
-    const previous = current === 0 ? images.length - 1 : current - 1;
-    setCurrent(previous);
-  };
-  const next = () => {
-    const next = current === images.length - 1 ? 0 : current + 1;
-    setCurrent(next);
-  };
   React.useEffect(() => {
     setCurrent(index);
   }, [index]);
 
+  const list = images.map((imgUrl: string, i) => {
+    const backgroundImage = `url(${s3Url}/${imgUrl})`;
+    return (
+      <div key={i} className='lightbox__image' style={{ backgroundImage }} />
+    );
+  });
+
   return ReactDOM.createPortal(
     <div className={`lightbox ${display ? 'lb-open' : 'lb-closed'}`}>
-      {renderCloseBtn(display, toggleLightbox, index)}
-      {display ? (
-        <div
-          className='lightbox__image'
-          style={{ backgroundImage: `url(${s3Url}/${images[current]})` }}
-        />
-      ) : null}
+      <div className='lightbox__close-btn' onClick={handleCloseLightbox}>
+        <div className={`${display && 'cross'}`} />
+        <div className={`${display && 'cross'}`} />
+        <div className={`${display && 'cross'}`} />
+      </div>
+      <div className='lightbox__images'>{list}</div>
+      {display && (
+        <div className='selected-lightbox-image'>{list[current]}</div>
+      )}
       <div className='lightbox__btns'>
-        <div onClick={() => prev()}>
-          <i className='fas fa-angle-left'></i>
-        </div>
-        <div onClick={() => next()}>
-          <i className='fas fa-angle-right'></i>
-        </div>
+        <i className='fas fa-angle-left' onClick={prev}></i>
+        <i className='fas fa-angle-right' onClick={next}></i>
       </div>
     </div>,
     document.querySelector('#lightbox')!
@@ -46,11 +47,3 @@ const Lightbox: React.FC = () => {
 };
 
 export default Lightbox;
-
-const renderCloseBtn = (bool: boolean, cb: Function, i: number) => (
-  <div className='lightbox__close-btn' onClick={() => cb(false, [], i)}>
-    <div className={`lightbox__bar ${bool ? 'cross' : ''}`}></div>
-    <div className={`lightbox__bar ${bool ? 'cross' : ''}`}></div>
-    <div className={`lightbox__bar ${bool ? 'cross' : ''}`}></div>
-  </div>
-);
